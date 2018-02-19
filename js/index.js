17636f2b234615005da6f31c58a68259ca0e4657
Bobby Hewitt <bobby.hewitt@w12studios.com>
2018-02-19 19:28:19 +0000

    initial commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+console.log('Helllo')
+// var ball = document.getElementById('ball')
+
+// ball.style.background= 'blue'
+
+// function moveBall(){
+//   ball.classList = ('topLeft')
+// }
+
+// var marginLeft = 0
+
+// function moveBall(){
+//   marginLeft += 1
+//   // console.log(marginLeft)
+//   ball.style.left = marginLeft + 'px'
+// }
+
+// setInterval(function(){
+//   moveBall()
+// }, 5)
